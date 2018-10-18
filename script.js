fc3aacde00d3b8374533da34df664c4261ad5ec2
Mehmood Bashir <39158675+mehmoodbashir28@users.noreply.github.com>
2018-10-18 17:40:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const option = {
+	duration: 1000
+}
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, option);
+  });
